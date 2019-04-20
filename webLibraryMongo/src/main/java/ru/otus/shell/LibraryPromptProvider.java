3276981d1c6538ee1142c02b3f1ff367d87a9cd9
EvgeniyUlanov <komrad1812@yandex.ru>
2019-04-20 12:16:29 +0300

    add shell to mongoLibrary

---
+++ webLibraryMongo/src/main/java/ru/otus/shell/LibraryPromptProvider.java
@@ -0,0 +1,14 @@
+package ru.otus.shell;
+
+import org.jline.utils.AttributedString;
+import org.springframework.shell.jline.PromptProvider;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LibraryPromptProvider implements PromptProvider {
+
+    @Override
+    public AttributedString getPrompt() {
+        return new AttributedString("web-libraryMongo-shell:>");
+    }
+}
