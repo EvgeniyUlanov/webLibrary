5a875d8b349c4604be3c2db925a9946d56bdf76a
EvgeniyUlanov <komrad1812@yandex.ru>
2019-05-08 02:40:52 +0300

    add hystrix

--- webLibrarySql/src/main/java/ru/otus/services/impl/AuthorServiceImpl.java
+++ webLibrarySql/src/main/java/ru/otus/services/impl/AuthorServiceImpl.java
@@ -1,5 +1,6 @@
 package ru.otus.services.impl;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.otus.domain.Author;
@@ -23,11 +24,13 @@ public class AuthorServiceImpl implements AuthorService {
     }
 
     @Override
+    @HystrixCommand(groupKey = "authorService", commandKey = "getAll", fallbackMethod = "fallBack")
     public List<Author> getAll() {
         return authorRepository.findAll();
     }
 
     @Override
+    @HystrixCommand(groupKey = "authorService", commandKey = "addNewAuthor", fallbackMethod = "fallBack")
     public boolean addNewAuthorWithName(String name) {
         try {
             authorRepository.save(new Author(name));
@@ -40,12 +43,18 @@ public class AuthorServiceImpl implements AuthorService {
     }
 
     @Override
+    @HystrixCommand(groupKey = "authorService", commandKey = "findByFullName", fallbackMethod = "fallBack")
     public List<Author> findByName(String authorName) {
         return Collections.singletonList(authorRepository.findByName(authorName).orElseThrow(() ->new EntityNotFoundException("author not found")));
     }
 
     @Override
+    @HystrixCommand(groupKey = "authorService", commandKey = "findByPartOfName", fallbackMethod = "fallBack")
     public List<Author> findByNameIgnoreCase(String authorName) {
         return authorRepository.findAuthorByNameIgnoreCase("%" + authorName + "%");
+    }
+
+    public String fallBack() {
+        return "wrong";
     }
 }
